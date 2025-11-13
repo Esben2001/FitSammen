12e56fb723e7756c471c2e54984b5be8445b6065
MatheotrucJ <10632122@ucn.dk>
2025-11-13 20:40:37 +0100

    sols setup with folders

---
+++ FitSammen_WebClient/FitSammen_WebClient/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using FitSammen_WebClient.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitSammen_WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
