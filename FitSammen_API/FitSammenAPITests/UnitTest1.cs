d55261f86d0420bed587212f7edf6e5b83d11a88
MatheotrucJ <10632122@ucn.dk>
2025-11-14 10:27:00 +0100

    test projects added

---
+++ FitSammen_API/FitSammenAPITests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FitSammenAPITests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ FitSammen_WebClient/WebClientTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebClientTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
