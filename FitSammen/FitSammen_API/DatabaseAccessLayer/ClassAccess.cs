7bf4e094c12a6c37d82707be7d243b5983a7a70b
MatheotrucJ <10632122@ucn.dk>
2025-11-17 14:11:43 +0100

    Refactor Class to use DateOnly and remove TrainingDate
    Replaced the TrainingDate class with DateOnly for the TrainingDate property in both API and WebClient Class models. Deleted the TrainingDate model files. Updated constructors and usages accordingly. Added GetAllClasses method to IClassAccess and its implementation in ClassAccess. Introduced a basic test class TestDBAccess.

--- FitSammen/FitSammen_API/DatabaseAccessLayer/ClassAccess.cs
+++ FitSammen/FitSammen_API/DatabaseAccessLayer/ClassAccess.cs
@@ -2,7 +2,7 @@
 
 namespace FitSammen_API.DatabaseAccessLayer
 {
-    public class ClassAccess
+    public class ClassAccess : IClassAccess
     {
         public ClassAccess(IConfiguration inConfiguration)
         {
@@ -12,5 +12,52 @@ namespace FitSammen_API.DatabaseAccessLayer
         }
 
         public string? ConnectionString { get; }
+
+        public IEnumerable<Class> GetAllClasses(ClassType classType, Location location, DateOnly endDate)
+        {
+            try
+            {
+                string sqlQuery = @"
+                SELECT
+                c.class_ID,
+                c.[name] AS className,
+                c.description,
+                c.startTime,
+                c.duration,
+                c.capacity,
+                c.memberCount,
+                c.trainingDate,
+
+                 ct.classType,
+
+                u.firstName AS employeeFirstName,
+                u.lastName AS employeeLastName,
+
+                r.roomName
+                FROM Class c
+                JOIN ClassType ct
+                    ON ct.classType_ID = c.classType_ID_FK
+                JOIN Room r
+                    ON r.room_ID = c.room_ID_FK
+                JOIN [Location] l
+                    ON l.location_ID = r.location_ID_FK
+                JOIN Employee e
+                    ON e.employeeUserNumber_FK = c.employeeUserNumber_FK
+                JOIN [User] u
+                    ON u.userNumber = e.employeeUserNumber_FK
+                WHERE
+                    c.trainingDate BETWEEN CAST(GETDATE() AS DATE)
+                        AND CAST('2025-11-20' AS DATE)
+                        AND l.location_ID = @LocationId
+                        AND ct.classType = @ClassType
+                ORDER BY
+                    c.trainingDate, c.startTime";
+            }
+            catch
+            {
+            }
+            return null;
+
+        }
     }
 }
